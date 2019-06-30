9d3a9c3723d1d4aaf6c281bf4ae9b3f51975162d
Bleen <bleengrg12345@gmail.com>
2019-06-30 21:34:53 +0545

    Add Authentication

--- app/src/main/java/com/example/notetakingapp/LoginForm.java
+++ app/src/main/java/com/example/notetakingapp/LoginForm.java
@@ -1,20 +1,40 @@
 package com.example.notetakingapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginForm extends AppCompatActivity {
 
     private TextView btn_signup;
+    private EditText etEmailLogin, etPassword;
+    private Button btnLogin;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        auth = FirebaseAuth.getInstance();
+
+        etEmailLogin = findViewById(R.id.etEmailLogin);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
 
         btn_signup = findViewById(R.id.btn_signup);
 
@@ -24,6 +44,59 @@ public class LoginForm extends AppCompatActivity {
 
                 Intent intent = new Intent(LoginForm.this, SignupForm.class);
                 startActivity(intent);
+
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = etEmailLogin.getText().toString();
+
+                String txt_password = etPassword.getText().toString();
+
+                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password))
+
+                {
+
+                    Toast.makeText(LoginForm.this, "All fields are required", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+
+                    auth.signInWithEmailAndPassword(txt_email, txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(task.isSuccessful())
+
+                                    {
+
+                                        Intent intent = new Intent(LoginForm.this, MainActivity.class);
+
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                                        startActivity(intent);
+
+                                        finish();
+
+                                    } else {
+
+                                        {
+
+                                            Toast.makeText(LoginForm.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+
+                                        }
+
+
+
+                                    }
+
+                                }
+                            });
+                }
+
 
             }
         });
